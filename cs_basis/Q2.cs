dbc7fc2c3a00a0d02968464cfd5f02cc5966a6e6
土田 剛生 <tsuchida@tarot.local>
2024-12-06 16:28:10 +0900

    Q2first

---
+++ cs_basis/Q2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("123");
+            Console.Write("456");
+            Console.Write("789");
+        }
+    }
+}
