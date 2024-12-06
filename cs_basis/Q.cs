deb1dfa7301657e53d592c9a6c092a2576c305ff
Your Name <you@example.com>
2024-12-06 17:27:19 +0900

    2-3first

---
+++ cs_basis/Q.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if(input != null)
+            Console.Write(input);
+        }
+    }
+}
