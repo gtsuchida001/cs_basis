dc12cd7f5ddd84917ce5d277f070871bcceb3ef2
nemu <tdgtjm44@gmail.com>
2024-12-08 20:35:29 +0900

    3-9first

--- cs_basis/Program.cs
+++ cs_basis/Program.cs
@@ -10,25 +10,15 @@ namespace Problem3_7
     {
         static void Main(string[] args)
         {
-            Console.Write("数値を入力");
-            int num = int.Parse(Console.ReadLine());
-            switch (num)
+            Console.Write("abcと入力してください：");
+            string inputString = Console.ReadLine();
+            if (inputString == "abc")
             {
-                case 1:
-                    Console.WriteLine("春");
-                    break;
-                case 2:
-                    Console.WriteLine("夏");
-                    break;
-                case 3:
-                    Console.WriteLine("秋");
-                    break;
-                case 4:
-                    Console.WriteLine("冬");
-                    break;
-                default:
-                    Console.WriteLine("エラーです。");
-                    break;
+                Console.WriteLine("abcです。");
+            }
+            else
+            {
+                Console.WriteLine("abcではありません。");
             }
         }
     }
