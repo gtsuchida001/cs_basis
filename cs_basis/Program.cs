3851d33d6f7e638e6e7249f697e452f6fc0ab36d
nemu <tdgtjm44@gmail.com>
2024-12-08 20:31:12 +0900

    3-8first

--- cs_basis/Program.cs
+++ cs_basis/Program.cs
@@ -12,13 +12,23 @@ namespace Problem3_7
         {
             Console.Write("数値を入力");
             int num = int.Parse(Console.ReadLine());
-            if (num % 2 == 0)
+            switch (num)
             {
-                Console.WriteLine("この数は偶数です。");
-            }
-            else
-            {
-                Console.WriteLine("この数は奇数です。");
+                case 1:
+                    Console.WriteLine("春");
+                    break;
+                case 2:
+                    Console.WriteLine("夏");
+                    break;
+                case 3:
+                    Console.WriteLine("秋");
+                    break;
+                case 4:
+                    Console.WriteLine("冬");
+                    break;
+                default:
+                    Console.WriteLine("エラーです。");
+                    break;
             }
         }
     }
