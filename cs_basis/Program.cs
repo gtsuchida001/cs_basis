53e32e0ce07b9eea6efd2ba80c3508196bfdf4df
nemu <tdgtjm44@gmail.com>
2024-12-08 18:38:10 +0900

    3-2 first

--- cs_basis/Program.cs
+++ cs_basis/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//test
+﻿ using System.Text;
+ using System.Threading.Tasks;
+ 
+ namespace Problem
+ {
+     class Program
+     {
+         static void Main(string[] args)
+         {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+                return;
+            if (!int.TryParse(input, out int number))
+            {
+                Console.WriteLine("Incorrect input");
+                return;
+            }
+            if (number != 4)
+                Console.Write("The number is not 4");
+         }
+     }
+ }
