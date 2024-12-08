7051e8fb18cb499d12739ef1459b6f6cf6bb1bf5
nemu <tdgtjm44@gmail.com>
2024-12-08 20:24:32 +0900

    3-6first

--- cs_basis/Program.cs
+++ cs_basis/Program.cs
@@ -1,23 +1,25 @@
-﻿ using System.Text;
- using System.Threading.Tasks;
- 
- namespace Problem
- {
-     class Program
-     {
-         static void Main(string[] args)
-         {
-            Console.Write("Enter a number: ");
-            string input = Console.ReadLine();
-            if (string.IsNullOrEmpty(input))
-                return;
-            if (!int.TryParse(input, out int number))
-            {
-                Console.WriteLine("Incorrect input");
-                return;
-            }
-            if (number > 30 || number <= 70)
-                Console.Write("The number is in the 30-70 range");
-         }
-     }
- }
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+                Console.WriteLine("a={0} b={1}\naはbよりも大きい", a, b);
+            else if (a < b)
+                Console.WriteLine("a={0} b={1}\naはbよりも小さい", b, a);
+            else
+                Console.WriteLine("a=b={0}\naはbと等しい" , a);
+        }
+    }
+}
