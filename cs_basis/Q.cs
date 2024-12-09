087ce52ea56c29fe1e8f625ef3e70d914288a796
Your Name <you@example.com>
2024-12-09 13:51:35 +0900

    4-8first

--- cs_basis/Q.cs
+++ cs_basis/Q.cs
@@ -24,22 +24,33 @@ namespace Problem
                 Console.Write("■");
                 i++;
             } while (i < input);*/
-            for (int a = 2; a <= 8; a += 2)
+            /*for (int a = 2; a <= 8; a += 2)
             {
                 if (a != 8)
                     Console.WriteLine("a={0}", a);
                 else
                     Console.Write("a=8");
-            }
-            /*int a = 2;
-            while (a <= 8)
-            {
-                if (a != 8)
-                    Console.WriteLine("a={0}", a);
-                else
-                    Console.Write("a=8");
-                a += 2;
             }*/
+            int index = 0;
+            string? inputString;
+            int result;
+            while (index >= 0)
+            {
+                inputString = Console.ReadLine();
+                if (!int.TryParse(inputString, out result) || inputString == null)
+                {
+                    Console.WriteLine("適切な値を入力してください。");
+                    return;
+                }
+                int input = int.Parse(inputString);
+                if (input >= 0)
+                    Console.WriteLine("{0}", input);
+                else if (input < 0)
+                {
+                    Console.Write("{0}", input);
+                    index--;
+                }
+            }
         }
     }
 }
