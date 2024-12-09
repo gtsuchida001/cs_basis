402b3a1c2dd4851d8e3b0ed0e3c04c094b41ed7e
Your Name <you@example.com>
2024-12-09 11:06:01 +0900

    3-6second

--- cs_basis/Program.cs
+++ cs_basis/Program.cs
@@ -4,22 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Problem3_6
+namespace Problem
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.Write("a=");
-            int a = int.Parse(Console.ReadLine());
+            string? inputString = Console.ReadLine();
+            if (!int.TryParse(inputString, out int result) || inputString == null)
+                return;
+            int inputa = int.Parse(inputString);
             Console.Write("b=");
-            int b = int.Parse(Console.ReadLine());
-            if (a > b)
-                Console.WriteLine("a={0} b={1}\naはbよりも大きい", a, b);
-            else if (a < b)
-                Console.WriteLine("a={0} b={1}\naはbよりも小さい", b, a);
+            string? inputString2 = Console.ReadLine();
+            if (!int.TryParse(inputString2, out int result2) || inputString2 == null)
+                return;
+            int inputb = int.Parse(inputString);
+            if (inputa > inputb)
+                Console.WriteLine("a={0} b={1}\naはbよりも大きい", inputa, inputb);
+            else if (inputa < inputb)
+                Console.WriteLine("a={0} b={1}\naはbよりも小さい", inputa, inputb);
             else
-                Console.WriteLine("a=b={0}\naはbと等しい" , a);
+                Console.WriteLine("a=b={0}\naはbと等しい" , inputa);
         }
     }
 }
