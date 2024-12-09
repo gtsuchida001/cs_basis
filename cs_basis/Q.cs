347f9a275d16d22f447e4a923a7906ae6040e19f
Your Name <you@example.com>
2024-12-09 11:29:35 +0900

    3-14first

--- cs_basis/Q.cs
+++ cs_basis/Q.cs
@@ -10,26 +10,27 @@ namespace Problem
     {
         static void Main(string[] args)
         {
-            Console.Write("1から3の値を入力（1:グー 2:パー 3:チョキ):");
+            Console.Write("a,bに1から10までの数値を入力してください。");
             string? inputString = Console.ReadLine();
             if (!int.TryParse(inputString, out int result) || inputString == null)
                 return;
             int input = int.Parse(inputString);
-            switch (input)
+            Console.Write("a,bに1から10までの数値を入力してください。");
+            string? inputString2 = Console.ReadLine();
+            if (!int.TryParse(inputString2, out int result2) || inputString2 == null)
+                return;
+            int input2 = int.Parse(inputString2);
+            if (input <=0 || input2 <= 0 || input > 10 || input > 10)
             {
-                case 1:
-                    Console.WriteLine("グー");
-                    break;
-                case 2:
-                    Console.WriteLine("パー");
-                    break;
-                case 3:
-                    Console.WriteLine("チョキ");
-                    break;
-                default:
-                    Console.WriteLine("正しい値ではありません");
-                    break;
+                Console.WriteLine("1から10までの数値を入力してください。");
+                return;
             }
+            if (input == input2)
+                Console.WriteLine("aとbは同じ値です。");
+            else if (input < input2)
+                Console.WriteLine("aはbより小さい値です。");
+            else
+                Console.WriteLine("aはbより大きい値です。");
         }
     }
 }
