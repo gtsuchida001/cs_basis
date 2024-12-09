aef121ff38516c9de4e0aa5adfe27346d2c736a6
Your Name <you@example.com>
2024-12-09 17:06:24 +0900

    5-2first

--- cs_basis/Q.cs
+++ cs_basis/Q.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Problem
+namespace cs_basis
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double []a = { 0.2, -5.1, 3.2, 1.8};
             for (int i = 0; i < a.Length; i++)
                 Console.Write("a[{0}] = {1}  ", i, a[i]);
+            string t = Console.ReadLine();
         }
     }
 }
