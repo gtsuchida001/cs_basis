bade20763d1b9166454928ccff681b0be7216e0e
Your Name <you@example.com>
2024-12-09 17:32:16 +0900

    5-3first

--- cs_basis/Q.cs
+++ cs_basis/Q.cs
@@ -10,10 +10,38 @@ namespace cs_basis
     {
         static void Main()
         {
-            double []a = { 0.2, -5.1, 3.2, 1.8};
-            for (int i = 0; i < a.Length; i++)
-                Console.Write("a[{0}] = {1}  ", i, a[i]);
-            string t = Console.ReadLine();
+            int []data = new int[10];
+            for (int index = 0; index < data.Length; index++)
+            {
+                Random random = new Random();
+                data[index] = random.Next(1, 101);
+                Console.Write("{0} ", data[index]);
+            }
+            Console.WriteLine();
+            int i = 0;
+            Console.Write("奇数：");
+            while (i < 10){
+                {
+                    if (data[i] % 2 != 0){
+                        Console.Write("{0}", data[i]);
+                        if (i != 9)
+                            Console.Write(" ");
+                    }
+                }
+                i++;
+            }
+            Console.WriteLine();
+            i = 0;
+            Console.Write("偶数：");
+            while (i < 10){
+                if (data[i] % 2 == 0){
+                    Console.Write("{0}", data[i]);
+                    if (i != 9)
+                        Console.Write(" ");
+                }
+                i++;
+            }
+            int t = Console.Read();
         }
     }
 }
