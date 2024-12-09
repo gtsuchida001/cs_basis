74fb9e81ef2d8988e47a288505922c2f6839d9d6
Your Name <you@example.com>
2024-12-09 10:59:41 +0900

    3-10second

--- cs_basis/Program.cs
+++ cs_basis/Program.cs
@@ -11,9 +11,9 @@ namespace Problem3_7
         static void Main(string[] args)
         {
             Console.Write("文字列１を入力：");
-            string inputString1 = Console.ReadLine();
+            string? inputString1 = Console.ReadLine();
             Console.Write("文字列２を入力：");
-            string inputString2 = Console.ReadLine();
+            string? inputString2 = Console.ReadLine();
             if (inputString1 == inputString2)
             {
                 Console.WriteLine("２つの文字列は等しい");
