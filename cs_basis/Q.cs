a8413b9ed96c1fc38b566607323520d5ec54b059
Your Name <you@example.com>
2024-12-09 11:58:59 +0900

    3-17first

--- cs_basis/Q.cs
+++ cs_basis/Q.cs
@@ -10,31 +10,30 @@ namespace Problem
     {
         static void Main(string[] args)
         {
-            Console.Write("整数値を入力：");
+            Console.Write("整数を入力してください(1-100)：");
             string? inputString = Console.ReadLine();
             if (!int.TryParse(inputString, out int result) || inputString == null)
                 return ;
             int input = int.Parse(inputString);
-            
-            int p2 = input % 2;
-            int p3 = input % 3;
-            if (p2 == 0 && p3 == 0)
+
+            int index = input % 2;
+            if (input <= 100 && input >= 1)
             {
-                Console.WriteLine("2と3の公倍数です。");
-                return;
-            }
-            else if (p2 == 0)
-            {
-                Console.WriteLine("2の公倍数です。");
-                return;
-            }
-            else if (p3 == 0)
-            {
-                Console.WriteLine("3の公倍数です。");
-                return;
+                if (input <= 50)
+                {
+                    if (index == 0)
+                        Console.WriteLine("50以下の偶数です。");
+                    else
+                        Console.WriteLine("50以下です。");
+                    return ;
+                }
+                if (index == 0)
+                    Console.WriteLine("偶数です。");
+                return ;
             }
             else
-                Console.WriteLine("2でも3でも割り切れません");
+                Console.WriteLine("適切な値を入力してください。");
+            return ;
         }
     }
 }
