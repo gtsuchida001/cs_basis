2c3649013a89eb2d09deada4f76798d5ae7b1bcc
Your Name <you@example.com>
2024-12-10 10:45:41 +0900

    5-9first

--- cs_basis/Q.cs
+++ cs_basis/Q.cs
@@ -10,15 +10,17 @@ namespace cs_basis
     {
         static void Main()
         {
-            int []data = new int[5];
+            int []data = new int[10];
 
             List<int> over_0 = new List<int>();
             List<int> under_0 = new List<int>();
+            List<int> zero = new List<int>();
+
 
             for (int index = 0; index < data.Length; index++)
             {
                 Random random = new Random();
-                data[index] = random.Next(-10, 11);
+                data[index] = random.Next(0, 100);
                 Console.Write("{0}", data[index]);
                 if (data[index] != data[^1])
                     Console.Write(" ");
@@ -26,16 +28,21 @@ namespace cs_basis
             Console.WriteLine();
             for (int index = 0; index < data.Length; index++)
             {
-                if (data[index] > 0){
+                if (data[index] > 49){
                     over_0.Add(data[index]);
                 }
-                if (data[index] < 0)
+                if (data[index] < 50)
                 {
                     under_0.Add(data[index]);
                 }
+                if (data[index] == 0)
+                {
+                    zero.Add(data[index]);
+                }
             }
-            Console.WriteLine("0より大きい数：" + string.Join(" " , over_0));
-            Console.Write("0より小さい数：" + string.Join(" ", under_0));
+            Console.WriteLine("50以上の数：" + string.Join(" " , over_0));
+            Console.WriteLine("50未満の数：" + string.Join(" ", under_0));
+            Console.Write("0の数：" + string.Join(" ", zero));
             int t = Console.Read();
         }
     }
