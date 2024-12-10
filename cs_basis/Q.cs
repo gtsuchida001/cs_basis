05a1dbd9acc92de5509bdc4f9bf8972f053e0734
Your Name <you@example.com>
2024-12-10 10:47:24 +0900

    5-9first

--- cs_basis/Q.cs
+++ cs_basis/Q.cs
@@ -20,7 +20,7 @@ namespace cs_basis
             for (int index = 0; index < data.Length; index++)
             {
                 Random random = new Random();
-                data[index] = random.Next(0, 100);
+                data[index] = random.Next(0, 101);
                 Console.Write("{0}", data[index]);
                 if (data[index] != data[^1])
                     Console.Write(" ");
@@ -28,7 +28,7 @@ namespace cs_basis
             Console.WriteLine();
             for (int index = 0; index < data.Length; index++)
             {
-                if (data[index] > 49){
+                if (data[index] > 0){
                     over_0.Add(data[index]);
                 }
                 if (data[index] < 50)
@@ -41,7 +41,7 @@ namespace cs_basis
                 }
             }
             Console.WriteLine("50以上の数：" + string.Join(" " , over_0));
-            Console.WriteLine("50未満の数：" + string.Join(" ", under_0));
+            Console.WriteLine("50以下の数：" + string.Join(" ", under_0));
             Console.Write("0の数：" + string.Join(" ", zero));
             int t = Console.Read();
         }
