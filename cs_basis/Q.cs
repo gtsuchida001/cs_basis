3121696e68e9d15a65378c3af3bec7e86f25f846
Your Name <you@example.com>
2024-12-10 10:37:42 +0900

    5-8first

--- cs_basis/Q.cs
+++ cs_basis/Q.cs
@@ -10,40 +10,32 @@ namespace cs_basis
     {
         static void Main()
         {
-            int []data = new int[10];
+            int []data = new int[5];
+
+            List<int> over_0 = new List<int>();
+            List<int> under_0 = new List<int>();
+
             for (int index = 0; index < data.Length; index++)
             {
                 Random random = new Random();
-                data[index] = random.Next(1, 11);
+                data[index] = random.Next(-10, 11);
                 Console.Write("{0}", data[index]);
                 if (data[index] != data[^1])
                     Console.Write(" ");
             }
             Console.WriteLine();
-            int i = 0;
-            Console.Write("3の倍数：");
-            while (i < data.Length)
+            for (int index = 0; index < data.Length; index++)
             {
-                if (data[i] % 3 == 0){
-                    Console.Write("{0}", data[i]);
-                    if (i != data.Length - 1)
-                        Console.Write(" ");
+                if (data[index] > 0){
+                    over_0.Add(data[index]);
                 }
-                i++;
-            }
-            i = 0;
-            Console.WriteLine();
-            Console.Write("3の倍数以外の数:");
-            while (i < data.Length)
-            {
-                if (data[i] % 3 != 0){
-                    Console.Write("{0}", data[i]);
-                    if (i != data.Length - 1)
-                        Console.Write(" ");
+                if (data[index] < 0)
+                {
+                    under_0.Add(data[index]);
                 }
-                i++;
             }
-
+            Console.WriteLine("0より大きい数：" + string.Join(" " , over_0));
+            Console.Write("0より小さい数：" + string.Join(" ", under_0));
             int t = Console.Read();
         }
     }
