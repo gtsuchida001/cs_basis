ada6e0c6bbcd13aee57055f28a6c23a2b55c6c6f
Your Name <you@example.com>
2024-12-10 13:46:13 +0900

    6-2first

--- cs_basis/Program.cs
+++ cs_basis/Program.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Problem6_1;
 
-namespace Problem6_1
+namespace Problem6_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MinMax m = new MinMax();
-            int a = 4, b = 2 ,c= 0;
-            Console.WriteLine("{0}Ç∆{1}Ç∆{2}ÇÃÇ§ÇøÅAç≈ëÂÇÃÇ‡ÇÃÇÕ{3}", a, b, c,m.Max(a, b,c));
-            Console.WriteLine("{0}Ç∆{1}Ç∆{2}ÇÃÇ§ÇøÅAç≈è¨ÇÃÇ‡ÇÃÇÕ{3}", a, b, c,m.Min(a, b,c));
+            Calc c = new Calc();
+            double a = 4.1, b = 2.3;
+            Console.WriteLine($"{a} + {b} = {Math.Round(c.Sum(a, b), 2)}");
+            Console.WriteLine($"{a} - {b} = {Math.Round(c.Sub(a, b), 2)}");
+            Console.WriteLine($"{a} * {b} = {Math.Round(c.Mul(a, b), 2)}");
+            Console.WriteLine($"{a} / {b} = {Math.Round(c.Div(a, b), 2)}");
+            //Console.WriteLine($"{a} / {b} = {c.Div(a, b)}");
         }
     }
 }
