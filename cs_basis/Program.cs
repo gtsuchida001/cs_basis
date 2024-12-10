ec3a7ece4e42ead614a7366add36ebcf3fca6942
Your Name <you@example.com>
2024-12-10 16:29:27 +0900

    8-1first

--- cs_basis/Program.cs
+++ cs_basis/Program.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Problem7_2;
 
-namespace Problem7_3
+namespace cs_basis
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Calculation c = new Calculation();
-            c.Number1 = 8;    //  一つ目の数をセット
-            c.Number2 = 9;    //  二つ目の数をセット
-            //  二つの数の和を表示
-            Console.WriteLine("{0} + {1} = {2}", c.Number1, c.Number2, c.Add());
-            //  二つの数の差を表示
-            Console.WriteLine("{0} - {1} = {2}", c.Number1, c.Number2, c.Sub());
+            int max = 100;
+
+            for (int a = 1; a <= max; a++)
+            {
+                for (int b = a; b <= max; b++)
+                {
+                    for (int c = b; c <= max; c++)
+                    {
+                        if (a * a + b * b == c * c)
+                        {
+                            Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
