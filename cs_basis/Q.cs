dc71b47d52865d90110653f270e7d430cfbbbe7d
Your Name <you@example.com>
2024-12-10 09:42:31 +0900

    5-4first]

--- cs_basis/Q.cs
+++ cs_basis/Q.cs
@@ -10,37 +10,38 @@ namespace cs_basis
     {
         static void Main()
         {
-            int []data = new int[10];
+            int []data = new int[100];
             for (int index = 0; index < data.Length; index++)
             {
                 Random random = new Random();
-                data[index] = random.Next(1, 101);
-                Console.Write("{0} ", data[index]);
+                data[index] = random.Next(1, 11);
+                Console.Write("{0}", data[index]);
+                if (data[index] != data[^1])
+                    Console.Write(" ");
             }
             Console.WriteLine();
             int i = 0;
-            Console.Write("奇数：");
-            while (i < 10){
-                {
-                    if (data[i] % 2 != 0){
-                        Console.Write("{0}", data[i]);
-                        if (i != 9)
-                            Console.Write(" ");
-                    }
-                }
-                i++;
-            }
-            Console.WriteLine();
-            i = 0;
-            Console.Write("偶数：");
-            while (i < 10){
-                if (data[i] % 2 == 0){
+            Console.Write("50以上の数：");
+            while (i < data.Length)
+            {
+                if (data[i] >= 50)
                     Console.Write("{0}", data[i]);
-                    if (i != 9)
-                        Console.Write(" ");
-                }
+                if (i != data.Length - 1)
+                    Console.Write(" ");
                 i++;
             }
+            i = 0;
+            Console.WriteLine();
+            Console.Write("50未満の数：");
+            while (i < data.Length)
+            {
+                if (data[i] < 50)
+                    Console.Write("{0}", data[i]);
+                if (i != data.Length - 1)
+                    Console.Write(" ");
+                i++;
+            }
+
             int t = Console.Read();
         }
     }
